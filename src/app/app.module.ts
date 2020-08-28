67e9f13a6dc8b9c5f07e274f972f34a4035547d4
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-08-28 16:21:26 +0300

    add github-search component

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GithubSearchComponent } from './github-search/github-search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GithubSearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
