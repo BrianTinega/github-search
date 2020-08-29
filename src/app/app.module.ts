818eaddd2a2e61050bd865d106c267048beb91ca
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-08-29 10:41:05 +0300

    add about and navigation-bar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { GithubSearchComponent } from './github-search/github-search.component';
 import { FormGithubSearchComponent } from './form-github-search/form-github-search.component';
 import { GithubSearchFormComponent } from './github-search-form/github-search-form.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubSearchComponent,
     FormGithubSearchComponent,
-    GithubSearchFormComponent
+    GithubSearchFormComponent,
+    NavigationBarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
