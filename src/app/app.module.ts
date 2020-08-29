8c4b711eac0d295084d5f1b600e23623c111b107
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-08-29 11:16:34 +0300

    add router and HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubSearchComponent } from './github-search/github-search.component';
-import { FormGithubSearchComponent } from './form-github-search/form-github-search.component';
+
 import { GithubSearchFormComponent } from './github-search-form/github-search-form.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { AboutComponent } from './about/about.component';
@@ -13,14 +14,15 @@ import { AboutComponent } from './about/about.component';
   declarations: [
     AppComponent,
     GithubSearchComponent,
-    FormGithubSearchComponent,
+    
     GithubSearchFormComponent,
     NavigationBarComponent,
     AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
